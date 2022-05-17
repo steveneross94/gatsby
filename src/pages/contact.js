81e537ee8ec5df492fcb77551817ff965f161534
Steve Ross <ross.steven@me.com>
2022-05-16 22:34:08 -0400

    remove email

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,49 +1,51 @@
-import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import Layout from '../components/layout'
-import Head from '../components/head'
-import linkStyles from '../components/header.module.scss'
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Layout from "../components/layout"
+import Head from "../components/head"
+import linkStyles from "../components/header.module.scss"
 
 function ContactPage() {
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata {
-                    portfolio,
-                    linkedIn,
-                    email,
-                    twitter
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          portfolio
+          linkedIn
+          email
+          twitter
         }
-    `)
+      }
+    }
+  `)
 
-    const info = data.site.siteMetadata
+  const info = data.site.siteMetadata
 
-    return (
-        <Layout>
-            <Head title="Contact" />
-            <h1>My Contact Info</h1>
-            <ul>
-                <li style={{listStyleType: 'none'}}>
-                    Email: <a href={`mailto:` + info.email} className={linkStyles.linkItem}>
-                        {info.email}</a>
-                </li>
-                <li style={{listStyleType: 'none'}}>
-                    LinkedIn: <a href={info.linkedIn} className={linkStyles.linkItem}>
-                        {info.linkedIn}</a>
-                </li>
-                <li style={{listStyleType: 'none'}}>
-                    Portfolio: <a href={info.portfolio} className={linkStyles.linkItem}>
-                        {info.portfolio}</a>
-                </li>
-                <li style={{listStyleType: 'none'}}>
-                    Twitter: <a href={info.twitter} className={linkStyles.linkItem}>
-                        {info.twitter}</a>
-                </li>
-            </ul>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head title="Contact" />
+      <h1>My Contact Info</h1>
+      <ul>
+        <li style={{ listStyleType: "none" }}>
+          LinkedIn:{" "}
+          <a href={info.linkedIn} className={linkStyles.linkItem}>
+            {info.linkedIn}
+          </a>
+        </li>
+        <li style={{ listStyleType: "none" }}>
+          Portfolio:{" "}
+          <a href={info.portfolio} className={linkStyles.linkItem}>
+            {info.portfolio}
+          </a>
+        </li>
+        <li style={{ listStyleType: "none" }}>
+          Twitter:{" "}
+          <a href={info.twitter} className={linkStyles.linkItem}>
+            {info.twitter}
+          </a>
+        </li>
+      </ul>
+    </Layout>
+  )
 }
 
 export default ContactPage
